3c6193352d757941310d6f28f023af1bfc0099fb
devika0999 <krishnadevika445@gmail.com>
2022-10-12 20:24:01 +0530

    Quotes app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { QuotesComponent } from './quotes/quotes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    QuotesComponent
   ],
   imports: [
     BrowserModule,
